ce209074b7298fcdf322cdb7b434cd5a3df51779
maribel <martinezmaribel1994@gmail.com>
2020-08-14 11:30:41 -0300

    prod arreglado

--- ShopProduct/src/main/java/ar/com/gl/shop/product/services/ProductService.java
+++ ShopProduct/src/main/java/ar/com/gl/shop/product/services/ProductService.java
@@ -7,7 +7,7 @@ import ar.com.gl.shop.product.repositoryimpl.ProductRepositoryImpl;
 
 public interface ProductService {
 	
-	public void create(Product product);
+	public Product create(Product product);
 	public List<Product> findAll();
 	/**
 	 * Este metodo sirve para buscar un elemento por id pasado por parametro,
@@ -21,8 +21,8 @@ public interface ProductService {
 	 */
 	public Product findById(Long id, Boolean bool);
 	public Product updateById(Product product);
-	public void deleteById(Product theProduct);
-	public void  forceDeleteById(Product theProduct);
+	public Product softDelete(Product theProduct);
+	public Product  delete(Product theProduct);
 	public ProductRepositoryImpl getRepositoryImpl();
 	public List<Product> findAllDisabled();
 
