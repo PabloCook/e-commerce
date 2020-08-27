a8ddcc8f94cd1dffb1007da742680d3b8aa2175f
maribel <martinezmaribel1994@gmail.com>
2020-08-27 18:27:15 -0300

    agregado proyecto zuul

---
+++ ZuulGateway/src/main/java/ar/com/gl/zuul/ZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package ar.com.gl.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class);
+	}
+
+}
