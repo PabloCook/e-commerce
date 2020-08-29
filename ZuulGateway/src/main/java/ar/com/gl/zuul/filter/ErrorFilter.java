0947cf6847a6ac030f59e018f0dc8891aaa8b95f
maribel <martinezmaribel1994@gmail.com>
2020-08-29 16:08:35 -0300

    error filter

--- ZuulGateway/src/main/java/ar/com/gl/zuul/filter/ErrorFilter.java
+++ ZuulGateway/src/main/java/ar/com/gl/zuul/filter/ErrorFilter.java
@@ -1,5 +1,37 @@
 package ar.com.gl.zuul.filter;
 
-public class ErrorFilter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ErrorFilter extends ZuulFilter {
+	
+	Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return RequestContext.getCurrentContext().getThrowable() != null;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+	    Throwable throwable = RequestContext.getCurrentContext().getThrowable();
+        log.error("Exception was thrown in filters: ", throwable);
+        return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "error";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
 
 }
