383e7a5764d51bd2c465c58e191c0359789d3a95
maribel <martinezmaribel1994@gmail.com>
2020-08-29 13:05:53 -0300

    postfilter

---
+++ ZuulGateway/src/main/java/ar/com/gl/zuul/filter/PostFilter.java
@@ -0,0 +1,56 @@
+package ar.com.gl.zuul.filter;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.CharStreams;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class PostFilter extends ZuulFilter {
+
+	Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+
+		RequestContext context = RequestContext.getCurrentContext();
+		try (final InputStream responseDataStream = context.getResponseDataStream()) {
+
+			if (responseDataStream == null) {
+				log.info("BODY: {}", "");
+				return null;
+			}
+			log.info("\n----- RESPONSE-----\n");
+			String responseData = CharStreams.toString(new InputStreamReader(responseDataStream, "UTF-8"));
+			log.info("\nBODY: \n{} ",  responseData+"\n");
+
+			context.setResponseBody(responseData);
+		} catch (Exception e) {
+			throw new ZuulException(e, 500, e.getMessage());
+		}
+
+		return null;
+
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
