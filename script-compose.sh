3090b0c83289d2b42c4e9d16d93f25b5d000a623
thedefede <fedemartinezleiva@gmail.com>
2020-09-08 21:19:27 -0300

    agregado scrpit para compose

---
+++ script-compose.sh
@@ -0,0 +1,48 @@
+ #!bin/bash
+
+cd ConfigServer/
+echo -e "\nVerificando si no hay compose levantado\n"
+docker-compose down
+echo -e "\nLevantando compose\n"
+docker-compose up -d
+
+cd ..
+
+#!/bin/bash
+# 1. Create ProgressBar function
+# 1.1 Input is currentState($1) and totalState($2)
+function ProgressBar {
+# Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+# Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:                           
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+
+}
+
+# Variables
+_start=1
+
+# This accounts as the "totalState" variable for the ProgressBar function
+_end=100
+
+# Proof of concept
+echo -e  "\nSubiendo Config Server y configuando Eureka......\n"
+for number in $(seq ${_start} ${_end})
+do
+    sleep 0.2
+    ProgressBar ${number} ${_end}
+done
+printf "\nFinished!\n"
+echo -e "\nVerificando si no hay compose levantado\n"
+docker-compose down
+echo -e "\nLevantado compose\n"
+docker-compose up
+
